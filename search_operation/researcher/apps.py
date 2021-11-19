f29b6a6c4fb66a104a36b5b24de553d67b92653e
youyinnn <youyinnn@gmail.com>
2021-11-18 21:37:08 -0500

    D3 doc

--- search_operation/researcher/apps.py
+++ search_operation/researcher/apps.py
@@ -1,4 +1,10 @@
+import os
 from django.apps import AppConfig
 
 class ResearcherConfig(AppConfig):
     name = 'researcher'
+    
+    def ready(self):
+        if os.environ.get('RUN_MAIN') == 'true':
+            print('Django start up code')
+            # asyncio.run(ws.start_up())
